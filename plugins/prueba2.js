5ed5d3d3c0c3f829abe288e2c3e7e2a9448bfd9d
Deylin <deylinbo91@gmail.com>
2025-06-06 15:14:04 -0600

    Actualizar prueba2.js

--- plugins/prueba2.js
+++ plugins/prueba2.js
@@ -2,18 +2,16 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn }) => {
   try {
-    // Llamada a la API para obtener la imagen
     const response = await fetch('https://anime-xi-wheat.vercel.app/api/anime');
     const data = await response.json();
 
-    if (!data || !data.url) {
+    if (!data || !data.image) {
       throw new Error('No se recibió una URL válida de la API');
     }
 
-    
     await conn.sendMessage(
       m.chat,
-      { image: { url: data.url }, caption: 'Aquí tienes una imagen anime' },
+      { image: { url: data.image }, caption: 'Aquí tienes una imagen anime' },
       { quoted: m }
     );
   } catch (error) {
@@ -27,6 +25,6 @@ const handler = async (m, { conn }) => {
 
 handler.command = ['an'];
 handler.help = [''];
-handler.tags = ['an'];
+handler.tags = ['anime'];
 
 export default handler;
