75c9fdbc33bbea04a1cf5a26c3417ffdf4f177b8
Adrianmaze <anuardavid2728@gmail.com>
2025-06-06 12:35:19 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -241,7 +241,9 @@ return
 let _user = global.db.data && global.db.data.users && global.db.data.users[m.sender]
         
 const detectwhat = m.sender.includes('@lid') ? '@lid' : '@s.whatsapp.net';
-const isROwner = [...global.owner.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + detectwhat).includes(m.sender)
+const isROwner = [conn.decodeJid(global.conn.user.id),
+...global.owner.map(([number, tipo]) => number.replace(/[^0-9]/g, '') + (tipo === 'jid'? '@s.whatsapp.net': '@lid'))
+].includes(m.sender);
 const isOwner = isROwner || m.fromMe
 const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + detectwhat).includes(m.sender)
 //const isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + detectwhat).includes(m.sender)
